e4b22323044acd8df14647421e625fd4aed778ce
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2023-07-24 21:09:55 +0300

    first commit

---
+++ src/main/java/ru/questsfera/quest_reservation/QuestReservationApplication.java
@@ -0,0 +1,13 @@
+package ru.questsfera.quest_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/questsfera/quest_reservation/QuestReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.questsfera.quest_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
